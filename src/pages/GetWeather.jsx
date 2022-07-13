f950e5d407dd99fe856765ab334d477ce5ead259
EveKlieber <Kliebereveline@gmail.com>
2022-07-13 19:03:56 +0200

    material ui added and colors changed

--- src/pages/GetWeather.jsx
+++ src/pages/GetWeather.jsx
@@ -1,14 +1,16 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from '@mui/material/Stack';
 import React, { useState, useEffect } from "react";
 import { getWeather } from "../../helpers/helpers";
 
 export const GetWeather = () => {
   const [input, setInput] = useState("");
 
-	const handleClick = (e) => {
-		getWeather(input);
-	}
+  const handleClick = e => {
+    getWeather(input);
+  };
 
   return (
     <div className="page_weather">
@@ -16,18 +18,19 @@ export const GetWeather = () => {
 
       <TextField
         id="outlined-basic"
-        label="Outlined"
+        label="enter location"
         variant="outlined"
         onChange={e => {
           setInput(e.target.value);
           console.log(e.target.value);
         }}
       />
-
-			<button
-				onClick={handleClick}
-				>send
-				</button>
+      <Stack spacing={2} direction="row">
+        <Button variant="outlined" onClick={handleClick}>
+          send
+        </Button>
+      </Stack>
+   
     </div>
   );
 };
