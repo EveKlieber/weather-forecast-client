35b4fb78415c1280595faf500090ceb9552cbb36
EveKlieber <Kliebereveline@gmail.com>
2022-08-23 18:43:22 +0200

    sunset sunrise added

--- src/components/WeatherResult.jsx
+++ src/components/WeatherResult.jsx
@@ -1,4 +1,4 @@
-import WeatherCard from './WeatherCard';
+import Temperature from './Temperature';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -7,40 +7,116 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
 import NavigationIcon from '@mui/icons-material/Navigation';
+import { useState, useEffect } from 'react';
 
 const WeatherResult = ({resultWeather, city}) => {
+
+  console.log(new Date(resultWeather?.sys.sunset*1000))
+
+  const [sunSet, setSunSet] = useState("")
+  const [sunRise, setSunRise] = useState("")
+  
+  useEffect(() => {
+    let sunRiseTime = new Date(resultWeather?.sys.sunrise*1000)
+    let sunRiseHour = sunRiseTime.getHours()
+    let sunRiseMinute= sunRiseTime.getMinutes()
+    sunRiseTime = `${sunRiseHour} : ${sunRiseMinute}`
+    setSunRise(sunRiseTime)
+    let sunSetTime = new Date(resultWeather?.sys.sunset*1000)
+  }, [])
+
+
   return (
 
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ width: "400px"}} >
     <CardContent>
+
+      
       <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
         {city}
       </Typography>
-      <Typography variant="h3" component="div">
-      Temp: {resultWeather?.main.temp}°
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-      feels-like: {resultWeather?.main.feels_like}°
-      </Typography>
-      <Typography variant="body2">
-      {resultWeather?.wind.speed} kmh 
-        </Typography>
-        <NavigationIcon ></NavigationIcon>
-      {resultWeather?.wind.deg}
 
-      <Box sx={{ maxWidth: 50, maxHeight: 50}}>
-        <CardMedia
-        component="img"
-        image={`http://openweathermap.org/img/wn/${resultWeather?.weather[0].icon}@2x.png`}
-        alt="weather icon"
-        />
+      <Box 
+      display="flex"
+      gap="20px"
+      >
+      <Box>
+        <Temperature temp={resultWeather?.main.temp}></Temperature>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        feels-like: {resultWeather?.main.feels_like}°
+    
+        </Typography>
       </Box>
-        <br />
-        {'"a benevolent smile"'}
+
+      <Box>        
+        <Box sx={{ maxWidth: 50, maxHeight: 50}}>
+          <CardMedia
+          component="img"
+          image={`http://openweathermap.org/img/wn/${resultWeather?.weather[0].icon}@2x.png`}
+          alt="weather icon"
+          />
+        </Box>
+        {resultWeather?.weather[0].description}
+      </Box>
+
+      <Box>
+          <NavigationIcon style={{transform: `rotate(${resultWeather?.wind.deg}deg)`}}></NavigationIcon>
+          <Typography variant="body2">
+          {resultWeather?.wind.speed} kmh 
+          </Typography>
+        </Box>
+    </Box>
+
+  <Box
+    display="flex"
+    gap="20px"
+    justifyContent="space-between"
+    
+  >
+      <Box>
+        <Typography color="text.secondary">
+        {resultWeather?.main.humidity}%
+          </Typography>
+          <Typography  color="text.secondary">
+            Rel.hum.
+        </Typography>
+      </Box>
+
+      <Box>
+        <Typography color="text.secondary">
+        {resultWeather?.main.pressure}hPa
+          </Typography>
+          <Typography  color="text.secondary">
+            Pressure
+        </Typography>
+      </Box>
+    </Box>
+
+    <Box>
+
+   
+
+    <Typography  color="text.secondary">
+    {sunRise}            
+    </Typography>
+
+        <Typography  color="text.secondary">
+            sunRise
+        </Typography>
+
+
+
+
+
+
+
+    </Box>
+
+
+
     </CardContent>
-    <CardActions>
-      <Button size="small">Learn More</Button>
-    </CardActions>
+
+
   </Card>
 
   )
